5fd8e92c8486af5862021b2985eb8888446160c6
danniorra <131016530+danniorra@users.noreply.github.com>
2025-08-31 14:19:15 +0000

    Added a folder fo IA3

---
+++ IA3/peerwise-helper.py
@@ -0,0 +1,9 @@
+# The package 'random' is used in this code
+
+import random
+
+lower_range = int(input('Enter the lower end of the range: '))
+upper_range = int(input('Enter the upper end of the range: '))
+
+print(random.randint(lower_range, upper_range))
+print(random.randint(lower_range, upper_range))
